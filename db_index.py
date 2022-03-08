1e2fb45cf5be305abc9c0162ca03fca28274fdad
plzfgme <plzfgme@mail.ustc.edu.cn>
2022-03-08 21:05:43 +0800

    db_index: completed

--- db_index.py
+++ db_index.py
@@ -1,5 +1,6 @@
 from .fast import *
 from bitarray import bitarray
+from bitarray.util import ba2int, int2ba
 
 class DBIndexClient:
     def __init__(self, db_path: str, keys_path: str):
@@ -9,8 +10,7 @@ class DBIndexClient:
         return self.fast_client.gen_update_tokens(id, self._equal_keyword(table_name, field_name, val), op)
 
     def gen_insert_range_tokens(self, op: str, table_name: str, id: bytes, field_name: str, val: int, range_log_2: int):
-        val_bin = bitarray()
-        val_bin.frombytes(val.to_bytes(range_log_2 // 8, 'big'))
+        val_bin = int2ba(val, length=range_log_2, endian='big')
         tokens_list = []
         for i in range(len(val_bin)):
             tokens_list.append(self.fast_client.gen_update_tokens(id, self._range_keyword(table_name, field_name, val_bin[:i].tobytes()), op))
@@ -18,14 +18,31 @@ class DBIndexClient:
         return tokens_list
 
     def gen_search_equal_tokens(self, table_name: str, field_name: str, val: bytes):
-        return self.fast_client.gen_search_tokens(bytes(table_name, 'utf-8')+b':e:'+bytes(field_name, 'utf-8')+b':'+val)
+        return self.fast_client.gen_search_tokens(self._equal_keyword(table_name, field_name, val))
 
-    def gen_search_range_tokens(self, table_name: str, field_name: str, lower_bound: int, upper_bound: int):
+    def gen_search_range_tokens(self, table_name: str, field_name: str, a: int, b: int, range_log_2: int):
         cset = set()
-        rset = set()
         i = 0
+        a_bin = int2ba(a, range_log_2, 'big')
+        b_bin = int2ba(b, range_log_2, 'big')
+        while len(a_bin) != 0 and ba2int(a_bin) < ba2int(b_bin):
+            if a_bin[-1] == 1:
+                cset.add(a_bin)
+            if b_bin[-1] == 0:
+                cset.add(b_bin)
+            a_bin = int2ba(ba2int(a_bin) + 1, range_log_2, 'big')
+            b_bin = int2ba(ba2int(b_bin) - 1, range_log_2, 'big')
 
-        pass 
+            a_bin >>= 1
+            b_bin >>= 1
+            i += 1
+        if a_bin == b_bin:
+            cset.add(a_bin)
+        tokens_list = []
+        for val_bin in cset:
+            tokens_list.append(self.fast_client.gen_search_tokens(self._range_keyword(table_name, field_name, val_bin.tobytes())))
+        
+        return tokens_list
 
     def _equal_keyword(self, table_name: str, field_name: str, val: bytes):
         return bytes(table_name, 'utf-8')+b':e:'+bytes(field_name, 'utf-8')+b':'+val
@@ -40,10 +57,16 @@ class DBIndexServer:
     def insert_equal(self, tokens):
         self.fast_server.update(*tokens)
 
-    def insert_range(self, tokens):
-        for token_pair in tokens:
-            self.fast_server.update(*token_pair)
+    def insert_range(self, tokens_list):
+        for tokens in tokens_list:
+            self.fast_server.update(*tokens)
 
     def search_equal(self, tokens):
         return self.fast_server.search(*tokens)
 
+    def search_range(self, tokens_list):
+        rset = set()
+        for tokens in tokens_list:
+            rset.union(self.fast_server.search(*tokens))
+        
+        return rset
